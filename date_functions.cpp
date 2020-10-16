ec0a900228ae5413571bc2af1102c308f5b3ca5b
namnguyen1911 <namnh11@gmail.com>
2020-10-16 16:33:39 -0700

    update isLeapYear

--- date_functions.cpp
+++ date_functions.cpp
@@ -3,6 +3,14 @@
 DateFunction::DateFunction(): day(1), dayOfWeek(1), month(1), year(2000) 
 {}
 
-string DateFunction::monthToString(){
-    
+bool DateFunction:: isLeapYear(int year)
+{
+    if( year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+        return true;
+
 }
+
+string DateFunction::monthToString(){
+}
+
+
