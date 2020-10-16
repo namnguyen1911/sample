f08f52de86a74cbc497cdaa20766c68203ce4a35
namnguyen1911 <namnh11@gmail.com>
2020-10-16 16:18:07 -0700

    Update 1

--- date_functions.h
+++ date_functions.h
@@ -1,3 +1,22 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
+
+class DateFunction
+{
+    private:
+        int dayOfWeek;
+        int month;
+        int year;
+        int day;
+    public:
+        DateFunction();
+        string monthToString(int month);
+        string dayOfWeekToString(int dayOfWeek);
+        int dayCap(int month);
+        bool isLeapYear(int year);
+
+
+
+};
