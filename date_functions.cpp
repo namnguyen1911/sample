cf378384bd9aa223bc3bc33f4fd87daa56db8dcb
quocngo-git <ngoquoc.work@gmail.com>
2020-10-16 16:39:38 -0700

    Update date_functions.cpp

--- date_functions.cpp
+++ date_functions.cpp
@@ -12,19 +12,32 @@ bool DateFunction:: isLeapYear(int year)
 
 string DateFunction::monthToString(){
     switch(month){
-    case 1: return "Jannuary"; break;
-    case 2: return "February"; break;
-    case 3: return "March"; break;
-    case 4: return "April"; break;
-    case 5: return "May"; break;
-    case 6: return "June"; break;
-    case 7: return "July"; break;
-    case 8: return "August"; break;
-    case 9: return "September"; break;
-    case 10: return "October"; break;
-    case 11: return "November"; break;
-    case 12: return "December"; break;
-    default: return "Invalid Month"; break;
+        case 1: return "Jannuary"; break;
+        case 2: return "February"; break;
+        case 3: return "March"; break;
+        case 4: return "April"; break;
+        case 5: return "May"; break;
+        case 6: return "June"; break;
+        case 7: return "July"; break;
+        case 8: return "August"; break;
+        case 9: return "September"; break;
+        case 10: return "October"; break;
+        case 11: return "November"; break;
+        case 12: return "December"; break;
+        default: return "Invalid Month"; break;
+    }
+}
+
+string DateFunction::dayOfWeekToString(){
+    int DoW = dayOfWeek % 7;
+    switch (DoW){
+        case 1: return "Sunday"; break;
+        case 2: return "Monday"; break;
+        case 3: return "Tuesday"; break;
+        case 4: return "Wednesday"; break;
+        case 5: return "Thursday"; break;
+        case 6: return "Friday"; break;
+        case 0: return "Saturday"; break;
     }
 }
 
